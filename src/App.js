693ad67e849bc07278c23286b8627359582dfc87
Akhil Rangpariya <akhil.rangpariya@mlveda.com>
2023-04-12 14:09:13 +0530

    task1 v1 react-route component use for Browser navigation and single page app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./component/Home";
+import { About } from "./component/About";
+import { Navbar } from "./component/Navbar";
+import { OrderSummary } from "./component/OrderSummary";
+import { NoMatch } from "./component/NoMatch";
+import { Products } from "./component/Products";
+import { FeaturedProducts } from "./component/FeaturedProducts";
+import { NewProducts } from "./component/NewProducts";
+import { Users } from "./component/Users";
+import { UserDetails } from "./component/UserDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="users" element={<Users />} />
+        <Route path="users/:userId" element={<UserDetails />} />
+        {/* <Route path="users/2" element={<UserDetails />} /> */}
+        {/* <Route path="users/3" element={<UserDetails />} /> */}
+
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </>
   );
 }
 
